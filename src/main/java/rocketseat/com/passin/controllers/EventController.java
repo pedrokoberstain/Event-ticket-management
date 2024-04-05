5093e8a9ee87e362613e9da1572704ee71b7c5c6
Kober <pedrokoberstain2@gmail.com>
2024-04-05 18:58:27 -0400

    Feat: Criando endpoint para buscar os participantes por eventos

--- src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -7,25 +7,34 @@ import org.springframework.web.util.UriComponentsBuilder;
 import rocketseat.com.passin.dto.event.EventIdDTO;
 import rocketseat.com.passin.dto.event.EventRequestDTO;
 import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.dto.event.attendee.AttendeesListResponseDTO;
+import rocketseat.com.passin.services.AttendeeService;
 import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/events")
 public class EventController {
-    private final EventService service;
+    private final EventService eventService;
+    private final AttendeeService attendeeService;
 
     @GetMapping("/{id}")
     public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
-        EventResponseDTO event = this.service.getEventDetail(id);
+        EventResponseDTO event = this.eventService.getEventDetail(id);
         return ResponseEntity.ok(event);
     }
 
     @PostMapping
     public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
-        EventIdDTO eventIdDTO = this.service.createEvent(body);
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
         var uri = uriComponentsBuilder.path("/events/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
         return ResponseEntity.created(uri).body(eventIdDTO);
+    }
+
+    @GetMapping("/attendees/{id}")
+    public ResponseEntity<AttendeesListResponseDTO> getEventAttendee(@PathVariable String id){
+        AttendeesListResponseDTO attendeesListResponse = this.attendeeService.getEventsAttendee(id);
+        return ResponseEntity.ok(attendeesListResponse);
     }
 
 
