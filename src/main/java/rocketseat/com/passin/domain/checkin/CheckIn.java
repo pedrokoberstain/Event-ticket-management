ca6be7ee4686e144638d2c399d6efcc9a46284c5
Kober <pedrokoberstain2@gmail.com>
2024-04-05 18:57:02 -0400

    Fix: Correção nos nomes das tabelas, estavam convergentes

--- src/main/java/rocketseat/com/passin/domain/checkin/CheckIn.java
+++ src/main/java/rocketseat/com/passin/domain/checkin/CheckIn.java
@@ -10,21 +10,21 @@ import rocketseat.com.passin.domain.attendee.Attendee;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "checkins")
+@Table(name = "check_ins")
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CheckIn {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(name = "created_at")
-    private LocalDateTime createAt;
+    private LocalDateTime createdAt;
 
     @OneToOne
     @JoinColumn(name = "attendee_id", nullable = false)
     private Attendee attendee;
-
 }
