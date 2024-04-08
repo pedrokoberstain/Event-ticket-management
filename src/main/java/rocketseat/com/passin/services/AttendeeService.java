a16f8da9e79c93a83d33c88aaf8180f8bf8680dd
Kober <pedrokoberstain2@gmail.com>
2024-04-08 01:06:31 -0400

    fix: Removendo duplicação de código na classe de serviço de check-in

--- src/main/java/rocketseat/com/passin/services/AttendeeService.java
+++ src/main/java/rocketseat/com/passin/services/AttendeeService.java
@@ -7,12 +7,8 @@ import rocketseat.com.passin.domain.attendee.Attendee;
 import rocketseat.com.passin.domain.attendee.exceptions.AttendeeAlreadyExistException;
 import rocketseat.com.passin.domain.attendee.exceptions.AttendeeNotFoundException;
 import rocketseat.com.passin.domain.checkin.CheckIn;
-import rocketseat.com.passin.dto.attendee.AttendeeBadgeResponseDTO;
-import rocketseat.com.passin.dto.attendee.AttendeeDetails;
-import rocketseat.com.passin.dto.attendee.AttendeesListResponseDTO;
-import rocketseat.com.passin.dto.attendee.BadgeDTO;
+import rocketseat.com.passin.dto.attendee.*;
 import rocketseat.com.passin.repositories.AttendeeRepository;
-import rocketseat.com.passin.repositories.CheckinRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,29 +19,28 @@ import java.util.Optional;
 public class AttendeeService {
 
     private final AttendeeRepository attendeeRepository;
-    private final CheckinRepository checkInRepository;
+    private final CheckinService checkinService;
 
-    public List<Attendee> getAllAttendeesFromEvents(String EventId) {
-        return this.attendeeRepository.findByEventId(EventId);
-
+    public List<Attendee> getAllAttendeesFromEvent(String eventId) {
+        return this.attendeeRepository.findByEventId(eventId);
     }
 
     public AttendeesListResponseDTO getEventsAttendee(String eventId) {
-        List<Attendee> attendeeList = this.getAllAttendeesFromEvents(eventId);
+        List<Attendee> attendeeList = this.getAllAttendeesFromEvent(eventId);
 
         List<AttendeeDetails> attendeeDetailsList = attendeeList.stream().map(attendee -> {
-            Optional<CheckIn> checkIn = this.checkInRepository.findByAttendeeId(attendee.getId());
+            Optional<CheckIn> checkIn = this.checkinService.getCheckIn(attendee.getId());
             LocalDateTime checkedInAt = checkIn.<LocalDateTime>map(CheckIn::getCreatedAt).orElse(null);
             return new AttendeeDetails(attendee.getId(), attendee.getName(), attendee.getEmail(), attendee.getCreatedAt(), checkedInAt);
         }).toList();
+
         return new AttendeesListResponseDTO(attendeeDetailsList);
     }
 
     public void verifyAttendeeSubscription(String email, String eventId) {
         Optional<Attendee> isAttendeeRegistered = this.attendeeRepository.findByEventIdAndEmail(eventId, email);
-        if (isAttendeeRegistered.isPresent()) {
-            throw new AttendeeAlreadyExistException("Attendee already registered");
-        }
+        if (isAttendeeRegistered.isPresent()) throw new AttendeeAlreadyExistException("Attendee already registered");
+
     }
 
     public Attendee registerAttendee(Attendee newAttendee) {
@@ -53,14 +48,22 @@ public class AttendeeService {
         return newAttendee;
     }
 
+    public void checkInAttendee(String attendeeId) {
+        Attendee attendee = this.getAttendee(attendeeId);
+        this.checkinService.registerCheckIn(attendee);
+    }
+
+    private Attendee getAttendee(String attendeeId) {
+        return this.attendeeRepository.findById(attendeeId).orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with ID: " + attendeeId));
+    }
+
+
     public AttendeeBadgeResponseDTO getAttendeeBadge(String attendeeId, UriComponentsBuilder uriComponentsBuilder) {
-        Attendee attendee = this.attendeeRepository.findById(attendeeId).orElseThrow(() -> new AttendeeNotFoundException("Attendee not found with ID: " + attendeeId));
+        Attendee attendee = this.getAttendee(attendeeId);
 
         var uri = uriComponentsBuilder.path("/attendees/{attendeeId}/check-in").buildAndExpand(attendeeId).toUri().toString();
 
-        BadgeDTO badgeDTO = new BadgeDTO(attendee.getName(), attendee.getEmail(), uri, attendee.getEvent().getId());
-        return new AttendeeBadgeResponseDTO(badgeDTO);
-
-
+        AttendeeBadgeDTO attendeeBadgeDTO = new AttendeeBadgeDTO(attendee.getName(), attendee.getEmail(), uri, attendee.getEvent().getId());
+        return new AttendeeBadgeResponseDTO(attendeeBadgeDTO);
     }
 }
